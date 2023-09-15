4b9fd4b150a7ece6dee45ede916f422aca6458dd
ExceptedPrism3 <ben.ayoube@outlook.com>
2023-09-15 12:13:30 +0100

    X

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
-
 """
-Module to perfom simple queries on the model_state model
-using and ORM - SQLAlchemy
+All states via SQLAlchemy
 """
+from sys import argv
 from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import sys
-
-
-def connect_and_query(user: str, passwd: str, dbase: str) -> None:
-
-    """
-    Connect to the database and make queries using ORM
-    """
-    try:
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                               .format(user, passwd, dbase))
-        Session = sessionmaker(bind=engine)
-        state_session = Session()
-
-        lousiana = State(name="Louisiana")
-        state_session.add(lousiana)
-        state_session.commit()
-        print(lousiana.id)
-    except Exception as e:
-        return e
-
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    connect_and_query(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    new_obj = State(name='Louisiana')
+    session.add(new_obj)
+    session.commit()
+    print(new_obj.id)
+    session.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
-
 """
-Lists all cities from the cities table of database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <username> \
-                            <password> \
-                            <database-name>
+lists all cities from the database hbtn_0e_4_usa
 """
-import sys
-import MySQLdb as db
-
-
-def connect_and_query() -> None:
-
-    """Connect to the database and execute query"""
-    try:
-        cnx = db.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-        cursor = cnx.cursor(cursorclass=db.cursors.Cursor)
-        cursor.execute('SELECT city.id, city.name, state.name\
-                        FROM cities as city\
-                        INNER JOIN states as state\
-                        ON city.state_id = state.id\
-                        ORDER BY city.id ASC;')
-        cities = cursor.fetchall()
-
-        for city in cities:
-            print(city)
-
-        cursor.close()
-        cnx.close()
-    except Exception as e:
-        return (e)
-
-
 if __name__ == "__main__":
-    connect_and_query()
+
+    import MySQLdb
+    from sys import argv
+
+    conect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = conect.cursor()
+    cursor.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities
+    LEFT JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC""")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    conect.close()
