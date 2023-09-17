71a7cfce739071f4a4d1c6711fd4c68b9e2149a1
ExceptedPrism3 <ben.ayoube@outlook.com>
2023-09-18 00:10:46 +0100

    X

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+"""
+takes in an argument and displays all values in the states
+itable of hbtn_0e_0_usa where name matches the argument.
+"""
 import sys
-
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = ("SELECT * FROM states "
+             "WHERE BINARY name = '{}' ".format(sys.argv[4]))
+    cursor.execute(query)
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
     db.close()
